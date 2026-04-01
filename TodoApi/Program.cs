4d6860829664dc294205a4a687f7e2b9614352e2
TimaDyakonu <tima_dyakonu@mail.ru>
2026-04-01 12:15:35 +0300

    update Program.cs

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -4,12 +4,25 @@ using TodoApi.Hubs;
 
 var builder = WebApplication.CreateBuilder(args); // factory
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
+
 builder.Services.AddControllers();
 builder.Services.AddSignalR();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build(); // builder
+
+app.UseCors();
 
 using (var scope = app.Services.CreateScope())
 {
